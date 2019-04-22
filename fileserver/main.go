78371256a1e8f7dbd67fb19c9d6352daae74d844
nukesz <norbert.benczur@gmail.com>
2019-04-22 20:25:56 +0200

    Added markdown support

--- fileserver/main.go
+++ fileserver/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/russross/blackfriday"
+)
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/markdown", generateMarkdownHandler)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func generateMarkdownHandler(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
 }
