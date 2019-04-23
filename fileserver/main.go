7d134f1f0977a23066ac7c30aee7ef0f3ebc6570
nukesz <norbert.benczur@gmail.com>
2019-04-23 19:02:40 +0200

    Read port from Env variable

--- fileserver/main.go
+++ fileserver/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/russross/blackfriday"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/markdown", generateMarkdownHandler)
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func generateMarkdownHandler(rw http.ResponseWriter, r *http.Request) {
